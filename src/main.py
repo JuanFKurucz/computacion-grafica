f542b5610264dfe31d38cd735468e5c57f3560c6
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2020-11-03 19:58:55 -0300

    change main version

--- src/main.py
+++ src/main.py
@@ -1,14 +1,63 @@
-import random
-
 import pygame
 from pygame.locals import *
 
 from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GL.shaders import *
+from obj import *
 
-from obj import Object
+# Uso esta funcion para compilar de forma individual el codigo de cada componente del shader (vertex y fragment)
+# Le paso el path al archivo y el tipo de shader (GL_VERTEX_SHADER o GL_FRAGMENT_SHADER)
+def compileProgram(path, type):
+    # Leo el codigo fuente desde el archivo
+    sourceFile = open(path, "r")
+    source = sourceFile.read()
+    # Creo un shader vacio en memoria de video, del tipo indicado
+    # En la variable shader queda almacenado un indice que nos va a permitir identificar este shader de ahora en mas
+    shader = glCreateShader(type)
+    # Le adjunto el codigo fuente leido desde el archivo
+    glShaderSource(shader, source)
+    # Intento compilarlo
+    glCompileShader(shader)
+    # Con la funcion glGelShaderiv puedo obtener el estado del compilador de shaders
+    # En este caso le pido el stado de la ultima compilacion ejecutada
+    if glGetShaderiv(shader, GL_COMPILE_STATUS) != GL_TRUE:
+        # Si la compilacion falla, muestro el error y retorno 0 (shader nulo)
+        print(path + ": " + glGetShaderInfoLog(shader))
+        # Me aseguro de liberar los recursos que reserve en memoria de vide, ya que no los voy a usar
+        glDeleteShader(shader)
+        return 0
+    else:
+        return shader
 
-ang = 0.0
-addVal = 0.0
+
+# Esta funcion me permite crear un programa de shading completo, basado en un vertex y un fragment shader
+# Le paso el path a ambos codigos fuentes
+def createShader(vSource, fSource):
+    # Creo y compilo el vertex shader
+    vProgram = compileProgram(vSource, GL_VERTEX_SHADER)
+    # Creo y compilo el fragment shader
+    fProgram = compileProgram(fSource, GL_FRAGMENT_SHADER)
+    # Creo un programa de shading vacio en memoria de video
+    shader = glCreateProgram()
+    # Le adjunto el codigo objeto del vertex shader compilado
+    glAttachShader(shader, vProgram)
+    # Le adjunto el codigo objeto del fragment shader compilado
+    glAttachShader(shader, fProgram)
+    # Intento linkear el programa para generar un ejecutable en memoria de video
+    glLinkProgram(shader)
+    # Chequeo si la ejecucion del linkeo del programa fue exitosa
+    if glGetProgramiv(shader, GL_LINK_STATUS) != GL_TRUE:
+        # Si falla, imprimo el mensaje de error y libero los recursos
+        print(glGetProgramInfoLog(shader))
+        glDeleteProgram(shader)
+        return 0
+    # Una vez que el programa fue linkeado, haya sido exitoso o no, ya no necesito los shaders
+    # individuales compilados, asi que libero sus recursos
+    glDeleteShader(vProgram)
+    glDeleteShader(fProgram)
+
+    return shader
 
 
 def loadTexture(path):
@@ -44,94 +93,134 @@ def loadTexture(path):
     # devuelvo el identificador de la textura para que pueda ser usada mas adelante
     return texid
 
-luz = 0.2
-def changeAng():
-    global ang
-    global addVal
-    global luz
-    ang += addVal
-    luz+=addVal/1000
 
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
-def init(ancho, largo):
-    #Activo el manejo de texturas
+    model = Object.loadObj("./assets/knight_texturas.obj")
+
+    # Creo un programa de shading y guardo la referencia en la variable gouraud
+    gouraud = createShader("./assets/shaders/gouraud_vs.hlsl", "./assets/shaders/gouraud_fs.hlsl")
+
+    # Activo el manejo de texturas
     glEnable(GL_TEXTURE_2D)
-    #Activo la textura 0 (hay 8 disponibles)
+    # Activo la textura 0 (hay 8 disponibles)
     glActiveTexture(GL_TEXTURE0)
+    # Llamo a la funcion que levanta la textura a memoria de video
+    text = loadTexture("./assets/knight.png")
 
+    # Para el shader, me guardo una referencia a la variable que representa a la textura
+    unifTextura = glGetUniformLocation(gouraud, "textura")
 
-    glMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE, [1,1,1,1])
-    glMaterial(GL_FRONT_AND_BACK, GL_AMBIENT, [0.1,0.1,0.1,1])
-    glMaterial(GL_FRONT_AND_BACK, GL_SPECULAR, [1,1,1,1])
+    glMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE, [1, 1, 1, 1])
+    glMaterial(GL_FRONT_AND_BACK, GL_AMBIENT, [1, 1, 1, 1])
+    glMaterial(GL_FRONT_AND_BACK, GL_SPECULAR, [1, 1, 1, 1])
     glMaterial(GL_FRONT_AND_BACK, GL_SHININESS, 16)
 
     glEnable(GL_LIGHT0)
 
-    glShadeModel(GL_SMOOTH)
+    glLight(GL_LIGHT0, GL_DIFFUSE, [1, 1, 1, 1])
+    glLight(GL_LIGHT0, GL_AMBIENT, [0.1, 0.1, 0.1, 1])
+    glLight(GL_LIGHT0, GL_POSITION, [0, 0, 0, 1])
+    glLight(GL_LIGHT0, GL_SPECULAR, [1, 1, 1, 1])
 
-    glLight(GL_LIGHT0, GL_DIFFUSE, [1,1,1,1])
-    glLight(GL_LIGHT0, GL_POSITION, [0,0,0,1])
-    glLight(GL_LIGHT0, GL_AMBIENT, [0.1,0.1,0.1,1])
-    glLight(GL_LIGHT0, GL_SPECULAR, [1,1,1,1])
-
-    
     glEnable(GL_DEPTH_TEST)
 
     glMatrixMode(GL_PROJECTION)
-    glViewport(0,0,ancho,largo)
-    glFrustum(-1,1,-1,1,1,1000)
+    glViewport(0, 0, display[0], display[1])
+    glFrustum(-1, 1, -1, 1, 1, 1000)
 
+    ang = 0.0
+    mode = GL_FILL
+    zBuffer = True
+    bfc = False
+    bfcCW = True
+    light = False
+    end = False
 
-    
-def main():
-    global ang
-    global addVal
-    pygame.init()
-    cw = 800
-    ch = 600
-    display = (cw, ch)
-    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    vel = 0.0
-    box = Object.loadObj("./assets/knight_texturas.obj")
-    text = loadTexture("./assets/knight.png")
-    
-    box.attachTexture(text)
-
-    init(cw,ch)
-    while True:
+    while not end:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    glEnable(GL_LIGHTING)
-                    addVal = -10
-                elif event.key == pygame.K_RIGHT:
-                    glDisable(GL_LIGHTING)
-                    addVal = 10
+                end = True
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    addVal = 0
+                if event.key == pygame.K_m:
+                    if mode == GL_LINE:
+                        mode = GL_FILL
+                    else:
+                        mode = GL_LINE
+                    glPolygonMode(GL_FRONT_AND_BACK, mode)
+                if event.key == pygame.K_z:
+                    zBuffer = not zBuffer
+                    if zBuffer:
+                        glEnable(GL_DEPTH_TEST)
+                    else:
+                        glDisable(GL_DEPTH_TEST)
+                if event.key == pygame.K_b:
+                    bfc = not bfc
+                    if bfc:
+                        glEnable(GL_CULL_FACE)
+                    else:
+                        glDisable(GL_CULL_FACE)
+                if event.key == pygame.K_c:
+                    bfcCW = not bfcCW
+                    if bfcCW:
+                        glFrontFace(GL_CW)
+                    else:
+                        glFrontFace(GL_CCW)
+                if event.key == pygame.K_l:
+                    light = not light
+                    if light:
+                        # Con la tecla L habilito y deshabilito el shader
+                        glUseProgram(gouraud)
+                    else:
+                        glUseProgram(0)
 
-        glLight(GL_LIGHT0, GL_AMBIENT, [luz, luz, luz, 1])
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+                elif event.key == pygame.K_ESCAPE:
+                    end = True
 
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-        glTranslatef(0, 0, -75)
-        glRotatef(-90, 1, 0, 0)
-        glRotatef(-45, 0, 0, 1)
-        glRotatef(-22, 0, 1, 0)
-
-        changeAng()
+        glTranslatef(0.0, 0.0, -50)
+        glRotatef(ang, 0, 1, 0)
         glRotatef(ang, 0, 0, 1)
-        
-        box.draw()
+        glRotatef(ang, 1, 0, 0)
+        ang += 0.5
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        glEnableClientState(GL_VERTEX_ARRAY)
+        glEnableClientState(GL_NORMAL_ARRAY)
+        # Habilito el array de coordenadas de textura
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY)
+
+        glVertexPointer(3, GL_FLOAT, 0, model.vertexes)
+        glNormalPointer(GL_FLOAT, 0, model.normals)
+        # Paso la lista de coordenadas de textura para cada vertice
+        glTexCoordPointer(2, GL_FLOAT, 0, model.textures)
+
+        if light:
+            # Si estoy usando shaders, le digo que la textura es la que esta activa en la posicion 0 (de las 8 disponibles)
+            glUniform1i(unifTextura, 0)
+        # Cargo la textura "text" en la posicion activa (que es la 0 en este ejemplo)
+        glBindTexture(GL_TEXTURE_2D, text)
+
+        glDrawArrays(GL_TRIANGLES, 0, len(model.poligons))
+
+        # Luego de dibujar, desactivo la textura
+        glBindTexture(GL_TEXTURE_2D, 0)
+
+        glDisableClientState(GL_VERTEX_ARRAY)
+        glDisableClientState(GL_NORMAL_ARRAY)
+        glDisableClientState(GL_TEXTURE_COORD_ARRAY)
 
         pygame.display.flip()
 
+    # Cuando salgo del loop, antes de cerrar el programa libero todos los recursos creados
+    glDeleteProgram(gouraud)
     glDeleteTextures([text])
     pygame.quit()
     quit()
+
+
 main()
